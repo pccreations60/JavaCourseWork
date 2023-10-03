1b9fb4d496dd60d5b0a88d9754cb1139cee994eb
pccreations60 <63941769+pccreations60@users.noreply.github.com>
2023-10-03 12:18:19 -0400

    Add files via upload

---
+++ newProj/src/newProj/NewProj.java
@@ -0,0 +1,11 @@
+package newProj;
+
+public class NewProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Suck My Dick");
+		System.out.println("New Line");
+	}
+
+}
